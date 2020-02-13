c62dcca51daf3c73ac02a75e8819ea80897d1de8
CodexCodech <israel_200@outlook.com>
2020-02-13 15:54:33 -0400

    Adding img and using modal

--- client/main.js
+++ client/main.js
@@ -35,3 +35,33 @@ Template.myGallery.events({
   },
 
 });
+
+Template.Addimage.events({
+	'click .js-addImage'(event,instance){
+		console.log("Opening Modal..");
+	},
+	'click .js-closeTab'(event,instance){
+		console.log("closed..");
+	},
+	'click .js-savechanges'(event,instance){
+	var theTitle =$("#Title").val();
+	console.log("Saving Title:"+theTitle);
+	var thePath =$("#Path").val();
+	console.log("Saving Path:" +thePath);
+	var theDesc =$("#Desc").val();
+	console.log("Saving Description:" +theDesc);
+	
+		 imagesdb.insert({
+  		"title": theTitle,
+  		"path" : thePath,
+  		"desc" : theDesc
+  		});
+
+	  	console.log("saving..");
+	  	$("#addImageModal").modal("hide");
+	  	$("#Title").val("");
+	  	$("#Path").val("");
+	  	$("#Desc").val("");
+  	}
+});
+
