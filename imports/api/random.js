4a11ee5c24747e4a4a48befa7ff04415c2c403e6
CodexCodech <israel_200@outlook.com>
2020-05-14 10:37:11 -0400

    Updated Random # Generator

--- imports/api/random.js
+++ imports/api/random.js
@@ -1,7 +1,21 @@
 import { Random } from 'meteor/random' 
 
+uniqueNumber = function(){
+	const n = Math.floor(Random.fraction()*imagesdb.find().count());
+	return n;
+}
+
 Template.randomImg.helpers({
 	randImg(){
-		return imagesdb.findOne({});
-	}
+		console.log(uniqueNumber());
+		return imagesdb.find().fetch()[uniqueNumber()];
+	},
+	 userField(){ // user field information
+  	if(!(this.createdBy == undefined)){
+  		return true;
+  	}
+  	else{
+  		return false;
+  	}
+  }
 });
